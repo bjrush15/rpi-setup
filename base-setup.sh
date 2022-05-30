0f8970c9b55e99e30a7dabe9f66be870f38e86fd
Brian Rush <brianrush15@gmail.com>
2022-05-30 15:52:01 -0700

    Auto install updates, fixed .bashrc source location, add prints

--- base-setup.sh
+++ base-setup.sh
@@ -2,13 +2,17 @@
 
 CONFIG_DIR="configs"
 
+echo " -- INSTALLING BASE APT PACKAGES --"
 # install base tools
-sudo apt install vim
+sudo apt install -y vim
 
+
+echo " -- INSTALLING CONFIGS -- "
 # move dot files into home directory
 cp "$CONFIG_DIR/.bashrc" "$CONFIG_DIR/.bash_prompt" "$HOME"
 # get immediate use of new .bashrc
-source .bashrc
+source "$HOME/.bashrc"
 
+echo " -- INSTALLING DOCKER -- "
 # install docker via official install script
 curl -sSL https://get.docker.com | sh
