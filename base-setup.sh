ab9922647750e4cb25be5cb9bf78523a9963badb
Brian Rush <brianrush15@gmail.com>
2022-05-30 15:47:21 -0700

    Source .bashrc after loading to immediately start using it

--- base-setup.sh
+++ base-setup.sh
@@ -7,6 +7,8 @@ sudo apt install vim
 
 # move dot files into home directory
 cp "$CONFIG_DIR/.bashrc" "$CONFIG_DIR/.bash_prompt" "$HOME"
+# get immediate use of new .bashrc
+source .bashrc
 
 # install docker via official install script
 curl -sSL https://get.docker.com | sh
