ba8416da6517a6466bcf93e45505203977f06be4
Brian Rush <brianrush15@gmail.com>
2022-06-08 05:23:50 +0100

    Add dnsutils package for nslookup/dig for debug

--- base-setup.sh
+++ base-setup.sh
@@ -4,10 +4,13 @@ CONFIG_DIR="configs"
 
 echo " -- INSTALLING base apt packages --"
 # install base tools
-sudo apt install -y vim
+# vim - text editor
+# dnsutils - provides dig/nslookup for DNS debug
+sudo apt install -y vim dnsutils
 
-# install realtek rtl8814au drivers (Alfa AWUS1900 wireless card)
+# install realtek rtl8814au drivers (For Alfa AWUS1900 wireless card)
 # instructions from https://davidtavarez.github.io/2018/re4son_kernel_raspberry_pi/
+# custom kernel was not needed as of 6/7/22
 if [[ -f "/lib/modules/$(uname -r)/kernel/drivers/net/wireless/88XXau.ko" ]]; then
   echo " -- INSTALLING Realtek rtl8814au drivers -- "
   sudo apt install -y raspberrypi-kernel-headers bc
@@ -25,7 +28,11 @@ fi
 
 echo " -- INSTALLING configs -- "
 # move dot files into home directory
+# .bashrc - bash config file
+# .bash_prompt - sourced from .bashrc to set up a colorful prompt
 cp -v "$CONFIG_DIR/.bashrc" "$CONFIG_DIR/.bash_prompt" "$HOME"
+
+
 # get immediate use of new .bashrc
 source "$HOME/.bashrc"
 
