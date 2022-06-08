9c6ce9464426a5180eabcc70f0b4031c8af0aa69
Brian Rush <brianrush15@gmail.com>
2022-06-08 08:28:45 +0100

    Updated base setup - pretty printing!

--- base-setup.sh
+++ base-setup.sh
@@ -1,32 +1,79 @@
 #!/bin/bash
 
-CONFIG_DIR="configs"
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 
-echo " -- INSTALLING base apt packages --"
+# pretty print function
+# $SIGN - use to tell which symbol to prepend
+#         CONT     [*] (blue)
+#         GOOD     [+] (green)
+#         BAD      [-] (red)
+#         WARN     [!] (yellow)
+# $MESSAGE - the message to print. FIRST word should be in all caps
+#            FIRST word will also be yellow
+function echo() {
+  SIGN="$1"
+  ECHO="$(which echo)"
+
+  case "$SIGN" in
+    "INFO")
+      symbol="${CYAN}*"
+      shift
+      ;;
+    "GOOD")
+      symbol="${GREEN}+"
+      shift
+      ;;
+    "BAD")
+      symbol="${RED}-"
+      shift
+      ;;
+    "WARN")
+      symbol="${YELLOW}!"
+      shift
+      ;;
+    *)
+      symbol="${CYAN}*"
+  esac
+  MESSAGE="$@"
+
+  eval "$ECHO" -e "$($ECHO -e $MESSAGE | sed 's/ / ${CYAN}/; s/^/${CYAN}\[${symbol}${CYAN}\]\ ${YELLOW}/; s/${NOCOLOR}/${CYAN}/; s/$/${NOCOLOR}/')" 
+}
+
+
+
+CONFIG_DIR="configs"
+echo INFO "USING config dir $CONFIG_DIR"
+
+echo INFO "INSTALLING base apt packages"
 # install base tools
 # vim - text editor
 # dnsutils - provides dig/nslookup for DNS debug
-sudo apt install -y vim dnsutils
+# stress - simple CPU stress test tool (testing Glances)
+sudo apt install -y vim dnsutils stress
 
 # install realtek rtl8814au drivers (For Alfa AWUS1900 wireless card)
 # instructions from https://davidtavarez.github.io/2018/re4son_kernel_raspberry_pi/
 # custom kernel was not needed as of 6/7/22
 if [[ ! -f "/lib/modules/$(uname -r)/kernel/drivers/net/wireless/88XXau.ko" ]]; then
-  echo " -- INSTALLING Realtek rtl8814au drivers -- "
+  # Build prereqs
   sudo apt install -y raspberrypi-kernel-headers bc
 
   git clone https://github.com/aircrack-ng/rtl8812au "$HOME/rtl8812au"
   cd "$HOME/rtl8812au"
-  echo " -- BUILDING rtl8814au drivers"
+  echo INFO "BUILDING Realtek rtl8814au drivers"
   make RTL8814=1 ARCH=arm
-  echo " -- INSTALLING rtl8814au drivers"
+  echo GOOD "INSTALLING Realtek rtl8814au drivers"
   sudo make install RTL8814=1 ARCH=arm
-  cd ../
+  cd -
 else
-  echo " -- SKIPPING Realtek rtl8814au driver install - already installed -- "
+  echo WARN "SKIPPING Realtek rtl8814au driver install - already installed"
 fi
 
-echo " -- INSTALLING configs -- "
+echo INFO "INSTALLING configs"
 # move dot files into home directory
 # .bashrc - bash config file
 # .bash_prompt - sourced from .bashrc to set up a colorful prompt
@@ -39,8 +86,8 @@ source "$HOME/.bashrc"
 
 # install docker via official install script if needed
 if [[ -z `which docker` ]]; then
-  echo " -- INSTALLING docker -- "
+  echo GOOD "INSTALLING docker"
   curl -sSL https://get.docker.com | sh
 else
-  echo " -- SKIPPING docker installation - already installed -- "
+  echo WARN "SKIPPING docker installation - already installed"
 fi
