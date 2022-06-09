563388a15093523c5a1dae5b69082b2c33084c58
Brian Rush <brianrush15@gmail.com>
2022-06-09 08:22:00 +0100

    Removed unneeded params to docker run

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -d -it --rm -p 80:80 --net=host --name nginx-docker-master nginx-docker
+sudo docker run -d -it --rm --net=host --name nginx-docker-master nginx-docker
