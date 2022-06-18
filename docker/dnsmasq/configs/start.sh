7902e6b2b6eb2470ffd0f07cc93f8ecabd530315
Brian <brianrush15@gmail.com>
2022-06-18 21:34:48 +0100

    Updated dnsmasq for arm64, access to 192.168.70.0/24 subnet, better cleanup on shutdown

--- docker/dnsmasq/configs/start.sh
+++ docker/dnsmasq/configs/start.sh
@@ -13,10 +13,21 @@ sigterm_handler() {
 }
 
 cleanup (){
+  echo -e "${CYAN}[*] Cleaning up routes${NOCOLOR}"
+  ip route del 192.168.70.0/24
+  echo -e "${CYAN}[*] Resetting interface eth1${NOCLOR}"
+  ifdown eth1
+  ifup eth1
   echo -e "${GREEN}[+] Successfully exited!${NOCOLOR}"
 }
 
 trap 'sigterm_handler' TERM INT
+
+echo -e "${CYAN}[*] Setting eth1 settings${NOCOLOR}"
+ifdown eth1
+ifup eth1
+ip route add 192.168.70.0/24 via 192.168.70.1 dev eth1
+
 echo -e "${CYAN}[+] Starting dnsmasq${NOCOLOR}"
 dnsmasq --no-daemon --log-queries --log-facility=/dnsmasq.log &
 pid=$!
