b3f6cab7ac6034fb16c3a28176030c99ba667aaf
Brian <brianrush15@gmail.com>
2022-06-18 22:22:27 +0100

    Removed glances directory as no config is needed outside of docker-compose.yml

--- docker/glances/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo docker run -d --restart="always" -p 61208-61209:61208-61209 -e GLANCES_OPT="-w" -v /var/run/docker.sock:/var/run/docker.sock:ro --pid host --net=host --privileged --name glances-docker-master glances-docker
